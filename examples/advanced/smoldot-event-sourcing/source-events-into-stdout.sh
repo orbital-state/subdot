9acd7aa01e051ed1526f7b45b7980d413b142170
Yauhen Yakimovich <yauhen.yakimovich.mail@gmail.com>
2025-04-26 00:44:50 +0200

    upd

---
+++ examples/advanced/smoldot-event-sourcing/source-events-into-stdout.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# This script demonstrates how to use subdot to source events from a smoldot source into stdout
+
+subdot filter \
+    --source smoldot.wss://rpc.polkadot.io \
+    --query 'true' \
+    --target "stdout"
